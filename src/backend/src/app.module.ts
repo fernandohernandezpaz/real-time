12ed2cdb8f1c087e23723ac659ff3be5c6265ea1
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2024-01-08 11:28:14 -0600

    creating docker structure and backend project

---
+++ src/backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {OrderModule} from './order/order.module';
+import {MongooseModule} from '@nestjs/mongoose';
+
+@Module({
+    imports: [
+        MongooseModule.forRoot(process.env.MONGODB_URL),
+        OrderModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {
+}
